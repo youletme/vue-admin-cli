003c0e0a84520219e344b82005bac363caa60236
yujiajun <yu704198352@gmail.com>
2020-06-16 08:51:54 +0800

    增加界面生成功能

---
+++ temp/page.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <pagination-table
+      ref="{{lowerName}}-list"
+      :table-columns='tableColumns'
+      :get-list-url="'/sys/{{lowerName}}/list'"
+      :delete-url="''"
+      :saveOrUpdateUrl="'/sys/{{lowerName}}'"
+      :getFormDateUrl="'/sys/{{lowerName}}/view'"
+      :useDefultOperate="false"
+      :addOrUpdateDialogWidth="'30%'"
+      :onlyCanSaveAndChange="true"
+    >
+
+    </pagination-table>
+
+  </div>
+</template>
+
+<script>
+import PaginationTable from '@/components/pagination-table'
+
+export default {
+  data () {
+    return {
+      tableColumns: [
+        {
+          prop: 'id',
+          headerAlign: 'center',
+          align: 'center',
+          width: '80',
+          label: 'ID',
+          notInForm: true
+        }
+      ]
+
+    }
+  },
+  components: {
+    PaginationTable
+  },
+  methods: {
+  }
+}
+</script>
+
+<style>
+
+</style>
